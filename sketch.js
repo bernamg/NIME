10b4ab65f42e21ba6f95d3db63ba9562770c9a8c
bernamg <berna.graca99@gmail.com>
2021-11-30 16:22:46 +0000

    ss

--- sketch.js
+++ sketch.js
@@ -1,4 +1,7 @@
+
+
 let note;
+let count = 0;
 
 function preload(){
   noteIMG = loadImage("images/nota1.png");
@@ -14,8 +17,8 @@ function setup() {
 
   note = new Note();
   fullscreenButton = createButton('FullScreen');
-  fullscreenButton.position(windowWidth-300,20);
-  fullscreenButton.addClass('full');
+  fullscreenButton.position(windowWidth*0.9-(fullscreenButton.width/2),20);
+  fullscreenButton.addClass('botton');
   fullscreenButton.style('border', '1px solid #ec1840');
   fullscreenButton.touchStarted(goFullScreen);
   //rato
@@ -23,13 +26,13 @@ function setup() {
 
 
   playButton = createButton('Play Music');
-  playButton.addClass('full');
+  playButton.addClass('botton');
   playButton.style('border', '1px solid #23f702');
   playButton.position(windowWidth-170,windowHeight/3);
   playButton.center('horizontal');
   playButton.touchStarted(goFullScreen);
   //rato
-  playButton.mousePressed();
+  playButton.mousePressed(play);
 }
 
 
@@ -43,10 +46,36 @@ function goFullScreen() {
 }
 
 function draw() {
+  /*if(count==0){
+    clear();
+    print("if do zero");
+    background(0);
+    note.move();
+    note.show();
+  }
+  if(count==1){
+    clear();
+    background(0);
+    print("if do count=1");
+    backButton = createButton('Back');
+    backButton.addClass('botton');
+    backButton.style('border', '1px solid #23f702');
+    backButton.position(windowWidth-170,windowHeight/3);
+    backButton.center('horizontal');
+    backButton.touchStarted(goFullScreen);
+    //rato
+    backButton.mousePressed(test1);
+  }*/
   background(0);
-  note.move();
-  note.show();
+    note.move();
+    note.show();
 
 }
 
+function test(){
+  count=1;
+}
 
+function test1(){
+  count=0;
+}
